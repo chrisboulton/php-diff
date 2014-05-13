f18e3cba537b9ce437a4e903af0025e7cd41ebe1
Maciej Schmidt <macsch15@gmail.com>
2014-05-13 16:45:28 +0200

    Support for PHP 5.5 and above
    The /e modifier is deprecated

--- lib/Diff/Renderer/Html/Array.php
+++ lib/Diff/Renderer/Html/Array.php
@@ -177,7 +177,7 @@ class Diff_Renderer_Html_Array extends Diff_Renderer_Abstract
 		$lines = array_map(array($this, 'ExpandTabs'), $lines);
 		$lines = array_map(array($this, 'HtmlSafe'), $lines);
 		foreach($lines as &$line) {
-			$line = preg_replace('# ( +)|^ #e', "\$this->fixSpaces('\\1')", $line);
+			$line = preg_replace_callback('# ( +)|^ #', array($this, 'fixSpaces'), $line);
 		}
 		return $lines;
 	}
@@ -185,12 +185,12 @@ class Diff_Renderer_Html_Array extends Diff_Renderer_Abstract
 	/**
 	 * Replace a string containing spaces with a HTML representation using &nbsp;.
 	 *
-	 * @param string $spaces The string of spaces.
+	 * @param array $matches The string of spaces.
 	 * @return string The HTML representation of the string.
 	 */
-	function fixSpaces($spaces='')
+	function fixSpaces($matches)
 	{
-		$count = strlen($spaces);
+		$count = strlen($matches[0]);
 		if($count == 0) {
 			return '';
 		}
