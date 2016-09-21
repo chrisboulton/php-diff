2900dd97eb540488c0bc188a0f5b2b6224a41ddd
Petr Kotek <petr.kotek@bigcommerce.com>
2016-09-22 01:30:07 +0200

    Diff_Renderer_Html_Array: avoid using 'e' modified in preg_replace - use preg_replace_callback instead

--- lib/Diff/Renderer/Html/Array.php
+++ lib/Diff/Renderer/Html/Array.php
@@ -177,7 +177,7 @@ class Diff_Renderer_Html_Array extends Diff_Renderer_Abstract
 		$lines = array_map(array($this, 'ExpandTabs'), $lines);
 		$lines = array_map(array($this, 'HtmlSafe'), $lines);
 		foreach($lines as &$line) {
-			$line = preg_replace('# ( +)|^ #e', "\$this->fixSpaces('\\1')", $line);
+			$line = preg_replace_callback('# ( +)|^ #', array($this, 'fixSpaces'), $line);
 		}
 		return $lines;
 	}
@@ -185,11 +185,12 @@ class Diff_Renderer_Html_Array extends Diff_Renderer_Abstract
 	/**
 	 * Replace a string containing spaces with a HTML representation using &nbsp;.
 	 *
-	 * @param string $spaces The string of spaces.
+	 * @param string[] $matches Array with preg matches.
 	 * @return string The HTML representation of the string.
 	 */
-	function fixSpaces($spaces='')
+	private function fixSpaces(array $matches)
 	{
+		$spaces = $matches[1];
 		$count = strlen($spaces);
 		if($count == 0) {
 			return '';
