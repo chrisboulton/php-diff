0db511d21bde153303760e5733209ffb3a98f547
JBlond <leet31337@web.de>
2019-01-15 10:57:49 +0100

    add unit tests

---
+++ tests/Diff/Renderer/ArrayTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace Tests\Diff\Renderer\Html;
+use jblond\Autoloader;
+use jblond\Diff\Renderer\Html\HtmlArray;
+
+require "../../../lib/Autoloader.php";
+new Autoloader();
+
+class ArrayTest extends \PHPUnit_Framework_TestCase
+{
+	public function testRenderSimpleDelete()
+	{
+		$htmlRenderer = new HtmlArray();
+		$htmlRenderer->diff = new \jblond\Diff(
+			array('a'),
+			array()
+		);
+		$result = $htmlRenderer->render();
+		static::assertEquals(array(
+			array(
+				array(
+					'tag' => 'delete',
+					'base' => array(
+						'offset' => 0,
+						'lines' => array(
+							'a'
+						)
+					),
+					'changed' => array(
+						'offset' => 0,
+						'lines' => array()
+					)
+				)
+			)
+		), $result);
+	}
+	public function testRenderFixesSpaces()
+	{
+		$htmlRenderer = new HtmlArray();
+		$htmlRenderer->diff = new \jblond\Diff(
+			array('    a'),
+			array('a')
+		);
+		$result = $htmlRenderer->render();
+		static::assertEquals(array(
+			array(
+				array(
+					'tag' => 'replace',
+					'base' => array(
+						'offset' => 0,
+						'lines' => array(
+							'<del>&nbsp; &nbsp;</del>a',
+						)
+					),
+					'changed' => array(
+						'offset' => 0,
+						'lines' => array(
+							'<ins></ins>a'
+						)
+					)
+				)
+			)
+		), $result);
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+//require_once __DIR__ .
+require_once '../lib/Autoloader.php';
