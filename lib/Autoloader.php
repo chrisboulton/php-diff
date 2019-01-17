a85071851f969b5e39b370a97fe2364fcbce93f4
JBlond <leet31337@web.de>
2019-01-17 10:46:29 +0100

    brace on a new line

--- lib/Autoloader.php
+++ lib/Autoloader.php
@@ -11,14 +11,16 @@ class Autoloader
 	/**
 	 * Autoloader constructor.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 		spl_autoload_register(array($this, '__autoload'));
 	}
 
 	/**
 	 * @param string $class
 	 */
-	private function __autoload($class) {
+	private function __autoload($class)
+	{
 		$class = str_replace('\\', '/', $class); // revert path for old PHP on Linux
 		$dir = str_replace('\\', '/', __DIR__);
 		if(file_exists($dir . '/' . $class . '.php')) {
