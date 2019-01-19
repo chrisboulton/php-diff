f326877863bb97f37c237ae9098b228238aafdcd
JBlond <leet31337@web.de>
2019-01-19 17:04:53 +0100

    tabs vs space

--- lib/jblond/Diff/Renderer/Html/SideBySide.php
+++ lib/jblond/Diff/Renderer/Html/SideBySide.php
@@ -48,197 +48,197 @@ namespace jblond\Diff\Renderer\Html;
  */
 class SideBySide extends HtmlArray
 {
-	/**
-	 * Render a and return diff with changes between the two sequences
-	 * displayed side by side.
-	 *
-	 * @return string The generated side by side diff.
-	 */
-	public function render() : string
-	{
-		$changes = parent::render();
+    /**
+     * Render a and return diff with changes between the two sequences
+     * displayed side by side.
+     *
+     * @return string The generated side by side diff.
+     */
+    public function render() : string
+    {
+        $changes = parent::render();
 
-		$html = '';
-		if (empty($changes)) {
-			return $html;
-		}
+        $html = '';
+        if (empty($changes)) {
+            return $html;
+        }
 
-		$html .= $this->generateTableHeader();
+        $html .= $this->generateTableHeader();
 
-		foreach ($changes as $i => $blocks) {
-			if ($i > 0) {
-				$html .= $this->generateSkippedTable();
-			}
+        foreach ($changes as $i => $blocks) {
+            if ($i > 0) {
+                $html .= $this->generateSkippedTable();
+            }
 
-			foreach ($blocks as $change) {
-				$html .= '<tbody class="Change'.ucfirst($change['tag']).'">';
-				switch ($change['tag']) {
-					// Equal changes should be shown on both sides of the diff
-					case 'equal':
-						$html .= $this->generateTableRowsEqual($change);
-						break;
-					// Added lines only on the right side
-					case 'insert':
-						$html .= $this->generateTableRowsInsert($change);
-						break;
-					// Show deleted lines only on the left side
-					case 'delete':
-						$html .= $this->generateTableRowsDelete($change);
-						break;
-					// Show modified lines on both sides
-					case 'replace':
-						$html .= $this->generateTableRowsReplace($change);
-						break;
-				}
-				$html .= '</tbody>';
-			}
-		}
-		$html .= '</table>';
-		return $html;
-	}
+            foreach ($blocks as $change) {
+                $html .= '<tbody class="Change'.ucfirst($change['tag']).'">';
+                switch ($change['tag']) {
+                    // Equal changes should be shown on both sides of the diff
+                    case 'equal':
+                        $html .= $this->generateTableRowsEqual($change);
+                        break;
+                    // Added lines only on the right side
+                    case 'insert':
+                        $html .= $this->generateTableRowsInsert($change);
+                        break;
+                    // Show deleted lines only on the left side
+                    case 'delete':
+                        $html .= $this->generateTableRowsDelete($change);
+                        break;
+                    // Show modified lines on both sides
+                    case 'replace':
+                        $html .= $this->generateTableRowsReplace($change);
+                        break;
+                }
+                $html .= '</tbody>';
+            }
+        }
+        $html .= '</table>';
+        return $html;
+    }
 
-	/**
-	 * Generates a string representation of a predefined table and its head with
-	 * titles from options.
-	 *
-	 * @return string Html code representation of the table's header.
-	 */
-	private function generateTableHeader() : string
-	{
-		$html = '<table class="Differences DifferencesSideBySide">';
-		$html .= '<thead>';
-		$html .= '<tr>';
-		$html .= '<th colspan="2">'.$this->options['title_a'].'</th>';
-		$html .= '<th colspan="2">'.$this->options['title_b'].'</th>';
-		$html .= '</tr>';
-		$html .= '</thead>';
-		return $html;
-	}
+    /**
+     * Generates a string representation of a predefined table and its head with
+     * titles from options.
+     *
+     * @return string Html code representation of the table's header.
+     */
+    private function generateTableHeader() : string
+    {
+        $html = '<table class="Differences DifferencesSideBySide">';
+        $html .= '<thead>';
+        $html .= '<tr>';
+        $html .= '<th colspan="2">'.$this->options['title_a'].'</th>';
+        $html .= '<th colspan="2">'.$this->options['title_b'].'</th>';
+        $html .= '</tr>';
+        $html .= '</thead>';
+        return $html;
+    }
 
-	/**
-	 * Generates a string representation of empty table body.
-	 *
-	 * @return string Html code representing empty table body.
-	 */
-	private function generateSkippedTable() : string
-	{
-		$html = '<tbody class="Skipped">';
-		$html .= '<th>&hellip;</th><td>&#xA0;</td>';
-		$html .= '<th>&hellip;</th><td>&#xA0;</td>';
-		$html .= '</tbody>';
-		return $html;
-	}
+    /**
+     * Generates a string representation of empty table body.
+     *
+     * @return string Html code representing empty table body.
+     */
+    private function generateSkippedTable() : string
+    {
+        $html = '<tbody class="Skipped">';
+        $html .= '<th>&hellip;</th><td>&#xA0;</td>';
+        $html .= '<th>&hellip;</th><td>&#xA0;</td>';
+        $html .= '</tbody>';
+        return $html;
+    }
 
-	/**
-	 * Generates a string representation of one or more rows of a table of lines of text with no difference.
-	 *
-	 * @param array &$change Array with data about changes.
-	 * @return string Html code representing one or more rows of text with no difference.
-	 */
-	private function generateTableRowsEqual(&$change) : string
-	{
-		$html = "";
-		foreach ($change['base']['lines'] as $no => $line) {
-			$fromLine = $change['base']['offset'] + $no + 1;
-			$toLine = $change['changed']['offset'] + $no + 1;
-			$html .= '<tr>';
-			$html .= '<th>'.$fromLine.'</th>';
-			$html .= '<td class="Left"><span>'.$line.'</span>&#xA0;</td>';
-			$html .= '<th>'.$toLine.'</th>';
-			$html .= '<td class="Right"><span>'.$line.'</span>&#xA0;</td>';
-			$html .= '</tr>';
-		}
-		return $html;
-	}
+    /**
+     * Generates a string representation of one or more rows of a table of lines of text with no difference.
+     *
+     * @param array &$change Array with data about changes.
+     * @return string Html code representing one or more rows of text with no difference.
+     */
+    private function generateTableRowsEqual(&$change) : string
+    {
+        $html = "";
+        foreach ($change['base']['lines'] as $no => $line) {
+            $fromLine = $change['base']['offset'] + $no + 1;
+            $toLine = $change['changed']['offset'] + $no + 1;
+            $html .= '<tr>';
+            $html .= '<th>'.$fromLine.'</th>';
+            $html .= '<td class="Left"><span>'.$line.'</span>&#xA0;</td>';
+            $html .= '<th>'.$toLine.'</th>';
+            $html .= '<td class="Right"><span>'.$line.'</span>&#xA0;</td>';
+            $html .= '</tr>';
+        }
+        return $html;
+    }
 
-	/**
-	 * Generates a string representation of one or more rows of a table of lines, where new text was added.
-	 *
-	 * @param array &$change Array with data about changes.
-	 * @return string Html code representing one or more rows of added text.
-	 */
-	private function generateTableRowsInsert(&$change) : string
-	{
-		$html = "";
-		foreach ($change['changed']['lines'] as $no => $line) {
-			$toLine = $change['changed']['offset'] + $no + 1;
-			$html .= '<tr>';
-			$html .= '<th>&#xA0;</th>';
-			$html .= '<td class="Left">&#xA0;</td>';
-			$html .= '<th>'.$toLine.'</th>';
-			$html .= '<td class="Right"><ins>'.$line.'</ins>&#xA0;</td>';
-			$html .= '</tr>';
-		}
-		return $html;
-	}
+    /**
+     * Generates a string representation of one or more rows of a table of lines, where new text was added.
+     *
+     * @param array &$change Array with data about changes.
+     * @return string Html code representing one or more rows of added text.
+     */
+    private function generateTableRowsInsert(&$change) : string
+    {
+        $html = "";
+        foreach ($change['changed']['lines'] as $no => $line) {
+            $toLine = $change['changed']['offset'] + $no + 1;
+            $html .= '<tr>';
+            $html .= '<th>&#xA0;</th>';
+            $html .= '<td class="Left">&#xA0;</td>';
+            $html .= '<th>'.$toLine.'</th>';
+            $html .= '<td class="Right"><ins>'.$line.'</ins>&#xA0;</td>';
+            $html .= '</tr>';
+        }
+        return $html;
+    }
 
-	/**
-	 * Generates a string representation of one or more rows of a table of lines, where text was removed.
-	 *
-	 * @param array &$change Array with data about changes.
-	 * @return string Html code representing one or more rows of removed text.
-	 */
-	private function generateTableRowsDelete(&$change) : string
-	{
-		$html = "";
-		foreach ($change['base']['lines'] as $no => $line) {
-			$fromLine = $change['base']['offset'] + $no + 1;
-			$html .= '<tr>';
-			$html .= '<th>'.$fromLine.'</th>';
-			$html .= '<td class="Left"><del>'.$line.'</del>&#xA0;</td>';
-			$html .= '<th>&#xA0;</th>';
-			$html .= '<td class="Right">&#xA0;</td>';
-			$html .= '</tr>';
-		}
-		return $html;
-	}
+    /**
+     * Generates a string representation of one or more rows of a table of lines, where text was removed.
+     *
+     * @param array &$change Array with data about changes.
+     * @return string Html code representing one or more rows of removed text.
+     */
+    private function generateTableRowsDelete(&$change) : string
+    {
+        $html = "";
+        foreach ($change['base']['lines'] as $no => $line) {
+            $fromLine = $change['base']['offset'] + $no + 1;
+            $html .= '<tr>';
+            $html .= '<th>'.$fromLine.'</th>';
+            $html .= '<td class="Left"><del>'.$line.'</del>&#xA0;</td>';
+            $html .= '<th>&#xA0;</th>';
+            $html .= '<td class="Right">&#xA0;</td>';
+            $html .= '</tr>';
+        }
+        return $html;
+    }
 
-	/**
-	 * Generates a string representation of one or more rows of a table of lines, where text was partially modified.
-	 *
-	 * @param array &$change Array with data about changes.
-	 * @return string Html code representing one or more rows of modified.
-	 */
-	private function generateTableRowsReplace(&$change) : string
-	{
-		$html = "";
+    /**
+     * Generates a string representation of one or more rows of a table of lines, where text was partially modified.
+     *
+     * @param array &$change Array with data about changes.
+     * @return string Html code representing one or more rows of modified.
+     */
+    private function generateTableRowsReplace(&$change) : string
+    {
+        $html = "";
 
-		if (count($change['base']['lines']) >= count($change['changed']['lines'])) {
-			foreach ($change['base']['lines'] as $no => $line) {
-				$fromLine = $change['base']['offset'] + $no + 1;
-				$html .= '<tr>';
-				$html .= '<th>'.$fromLine.'</th>';
-				$html .= '<td class="Left"><span>'.$line.'</span>&#xA0;</td>';
-				if (!isset($change['changed']['lines'][$no])) {
-					$toLine = '&#xA0;';
-					$changedLine = '&#xA0;';
-				} else {
-					$toLine = $change['changed']['offset'] + $no + 1;
-					$changedLine = '<span>'.$change['changed']['lines'][$no].'</span>';
-				}
-				$html .= '<th>'.$toLine.'</th>';
-				$html .= '<td class="Right">'.$changedLine.'</td>';
-				$html .= '</tr>';
-			}
-		} else {
-			foreach ($change['changed']['lines'] as $no => $changedLine) {
-				if (!isset($change['base']['lines'][$no])) {
-					$fromLine = '&#xA0;';
-					$line = '&#xA0;';
-				} else {
-					$fromLine = $change['base']['offset'] + $no + 1;
-					$line = '<span>'.$change['base']['lines'][$no].'</span>';
-				}
-				$html .= '<tr>';
-				$html .= '<th>'.$fromLine.'</th>';
-				$html .= '<td class="Left"><span>'.$line.'</span>&#xA0;</td>';
-				$toLine = $change['changed']['offset'] + $no + 1;
-				$html .= '<th>'.$toLine.'</th>';
-				$html .= '<td class="Right">'.$changedLine.'</td>';
-				$html .= '</tr>';
-			}
-		}
+        if (count($change['base']['lines']) >= count($change['changed']['lines'])) {
+            foreach ($change['base']['lines'] as $no => $line) {
+                $fromLine = $change['base']['offset'] + $no + 1;
+                $html .= '<tr>';
+                $html .= '<th>'.$fromLine.'</th>';
+                $html .= '<td class="Left"><span>'.$line.'</span>&#xA0;</td>';
+                if (!isset($change['changed']['lines'][$no])) {
+                    $toLine = '&#xA0;';
+                    $changedLine = '&#xA0;';
+                } else {
+                    $toLine = $change['changed']['offset'] + $no + 1;
+                    $changedLine = '<span>'.$change['changed']['lines'][$no].'</span>';
+                }
+                $html .= '<th>'.$toLine.'</th>';
+                $html .= '<td class="Right">'.$changedLine.'</td>';
+                $html .= '</tr>';
+            }
+        } else {
+            foreach ($change['changed']['lines'] as $no => $changedLine) {
+                if (!isset($change['base']['lines'][$no])) {
+                    $fromLine = '&#xA0;';
+                    $line = '&#xA0;';
+                } else {
+                    $fromLine = $change['base']['offset'] + $no + 1;
+                    $line = '<span>'.$change['base']['lines'][$no].'</span>';
+                }
+                $html .= '<tr>';
+                $html .= '<th>'.$fromLine.'</th>';
+                $html .= '<td class="Left"><span>'.$line.'</span>&#xA0;</td>';
+                $toLine = $change['changed']['offset'] + $no + 1;
+                $html .= '<th>'.$toLine.'</th>';
+                $html .= '<td class="Right">'.$changedLine.'</td>';
+                $html .= '</tr>';
+            }
+        }
 
-		return $html;
-	}
+        return $html;
+    }
 }
--- lib/jblond/Diff/Renderer/RendererAbstract.php
+++ lib/jblond/Diff/Renderer/RendererAbstract.php
@@ -44,44 +44,44 @@ namespace jblond\Diff\Renderer;
  */
 abstract class RendererAbstract
 {
-	/**
-	 * @var object Instance of the diff class that this renderer is generating the rendered diff for.
-	 */
-	public $diff;
+    /**
+     * @var object Instance of the diff class that this renderer is generating the rendered diff for.
+     */
+    public $diff;
 
-	/**
-	 * @var array Array of the default options that apply to this renderer.
-	 */
+    /**
+     * @var array Array of the default options that apply to this renderer.
+     */
     protected $defaultOptions = array(
         'title_a' => 'Old Version',
         'title_b' => 'New Version',
     );
 
-	/**
-	 * @var array Array containing the user applied and merged default options for the renderer.
-	 */
-	protected $options = array();
+    /**
+     * @var array Array containing the user applied and merged default options for the renderer.
+     */
+    protected $options = array();
 
-	/**
-	 * The constructor. Instantiates the rendering engine and if options are passed,
-	 * sets the options for the renderer.
-	 *
-	 * @param array $options Optionally, an array of the options for the renderer.
-	 */
-	public function __construct(array $options = array())
-	{
-		$this->setOptions($options);
-	}
+    /**
+     * The constructor. Instantiates the rendering engine and if options are passed,
+     * sets the options for the renderer.
+     *
+     * @param array $options Optionally, an array of the options for the renderer.
+     */
+    public function __construct(array $options = array())
+    {
+        $this->setOptions($options);
+    }
 
-	/**
-	 * Set the options of the renderer to those supplied in the passed in array.
-	 * Options are merged with the default to ensure that there aren't any missing
-	 * options.
-	 *
-	 * @param array $options Array of options to set.
-	 */
-	public function setOptions(array $options)
-	{
-		$this->options = array_merge($this->defaultOptions, $options);
-	}
+    /**
+     * Set the options of the renderer to those supplied in the passed in array.
+     * Options are merged with the default to ensure that there aren't any missing
+     * options.
+     *
+     * @param array $options Array of options to set.
+     */
+    public function setOptions(array $options)
+    {
+        $this->options = array_merge($this->defaultOptions, $options);
+    }
 }
