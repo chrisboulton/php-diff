8e72ea79158e878b3c2e2d3dc36737614abe0f44
JBlond <leet31337@web.de>
2019-02-19 10:14:00 +0100

    increase version

--- lib/jblond/Diff/Renderer/Html/SideBySide.php
+++ lib/jblond/Diff/Renderer/Html/SideBySide.php
@@ -39,7 +39,7 @@ namespace jblond\Diff\Renderer\Html;
  * @author Chris Boulton <chris.boulton@interspire.com>
  * @copyright (c) 2009 Chris Boulton
  * @license New BSD License http://www.opensource.org/licenses/bsd-license.php
- * @version 1.9
+ * @version 1.10
  * @link https://github.com/JBlond/php-diff
  */
 
