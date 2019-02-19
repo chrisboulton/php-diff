96f028c2e8ab3d6e3f5a773f3d82f9219a023536
JBlond <leet31337@web.de>
2019-02-19 21:51:44 +0100

    remove unsued function

--- lib/jblond/Diff/Renderer/Html/HtmlArray.php
+++ lib/jblond/Diff/Renderer/Html/HtmlArray.php
@@ -60,62 +60,6 @@ class HtmlArray extends RendererAbstract
     );
 
     /**
-    * From https://gist.github.com/stemar/8287074
-    * @param string $string The input string.
-    * @param string $replacement The replacement string.
-    * @param int $start If start is positive, the replacing will begin at the start'th offset into string.
-    * If start is negative, the replacing will begin at the start'th character from the end of string.
-    * @param int|null $length If given and is positive, it represents the length of the portion of string which is to
-     * be replaced. If it is negative, it represents the number of characters from the end of string at which to
-     * stop replacing. If it is not given, then it will default to strlen( string ); i.e. end the replacing at the
-     * end of string. Of course, if length is zero then this function will have the effect of inserting replacement
-     * into string at the given start offset.
-    * @return string|array The result string is returned. If string is an array then array is returned.
-    */
-    public function mbSubstrReplace(string $string, string $replacement, int $start, $length = null)
-    {
-        if (is_array($string)) {
-            $num = count($string);
-            // $replacement
-            if (is_array($replacement)) {
-                $replacement = array_slice($replacement, 0, $num);
-            } else {
-                $replacement = array_pad(array($replacement), $num, $replacement);
-            }
-
-            // $start
-            if (is_array($start)) {
-                $start = array_slice($start, 0, $num);
-                foreach ($start as $key => $value) {
-                    $start[$key] = is_int($value) ? $value : 0;
-                }
-            } else {
-                $start = array_pad(array($start), $num, $start);
-            }
-            // $length
-            if (!isset($length)) {
-                $length = array_fill(0, $num, 0);
-            } elseif (is_array($length)) {
-                $length = array_slice($length, 0, $num);
-                foreach ($length as $key => $value) {
-                    $length[$key] = isset($value) ? (is_int($value) ? $value : $num) : 0;
-                }
-            } else {
-                $length = array_pad(array($length), $num, $length);
-            }
-            // Recursive call
-            return array_map(array($this, 'mbSubstrReplace'), $string, $replacement, $start, $length);
-        }
-        preg_match_all('/./us', (string)$string, $smatches);
-        preg_match_all('/./us', (string)$replacement, $rmatches);
-        if ($length === null) {
-            $length = mb_strlen($string);
-        }
-        array_splice($smatches['0'], $start, $length, $rmatches[0]);
-        return join($smatches['0']);
-    }
-
-    /**
      * @param string|array $changes
      * @param SideBySide|Inline $object
      * @return string
