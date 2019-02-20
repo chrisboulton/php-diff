aa4cf3b074745018f117857fbabaa8bf8f6ba46e
JBlond <leet31337@web.de>
2019-02-20 14:33:35 +0100

    increase version number

--- lib/jblond/Diff.php
+++ lib/jblond/Diff.php
@@ -16,7 +16,7 @@ use jblond\Diff\SequenceMatcher;
  * @author Chris Boulton <chris.boulton@interspire.com>
  * @copyright (c) 2009 Chris Boulton
  * @license New BSD License http://www.opensource.org/licenses/bsd-license.php
- * @version 1.10
+ * @version 1.11
  * @link https://github.com/JBlond/php-diff
  */
 class Diff
