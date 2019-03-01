e0f68420b12d6f7278163cc780aaff05eee9a4b8
JBlond <leet31337@web.de>
2019-03-01 14:38:18 +0100

    PHP Mess Detector Avoid unused private methods such as '__autoload'. even if not correct

--- lib/Autoloader.php
+++ lib/Autoloader.php
@@ -13,19 +13,13 @@ class Autoloader
      */
     public function __construct()
     {
-        spl_autoload_register(array($this, '__autoload'));
-    }
-
-    /**
-     * @param string $class
-     */
-    private function __autoload($class)
-    {
-        $class = str_replace('\\', '/', $class); // revert path for old PHP on Linux
-        $dir = str_replace('\\', '/', __DIR__);
-        if (file_exists($dir . '/' . $class . '.php')) {
-             /** @noinspection PhpIncludeInspection */
-            require_once $dir . '/' . $class . '.php';
-        }
+        spl_autoload_register(function ($class) {
+            $class = str_replace('\\', '/', $class); // revert path for old PHP on Linux
+            $dir = str_replace('\\', '/', __DIR__);
+            if (file_exists($dir . '/' . $class . '.php')) {
+                /** @noinspection PhpIncludeInspection */
+                require_once $dir . '/' . $class . '.php';
+            }
+        });
     }
 }
