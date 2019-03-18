1cdd3dc69a731cef35813a3881e3fe358ea57c45
JBlond <leet31337@web.de>
2019-03-18 20:58:51 +0100

    prepare 1.13

--- lib/jblond/Diff/Renderer/Text/Context.php
+++ lib/jblond/Diff/Renderer/Text/Context.php
@@ -13,7 +13,7 @@ use jblond\Diff\Renderer\RendererAbstract;
  * @author Chris Boulton <chris.boulton@interspire.com>
  * @copyright (c) 2009 Chris Boulton
  * @license New BSD License http://www.opensource.org/licenses/bsd-license.php
- * @version 1.12
+ * @version 1.13
  * @link https://github.com/JBlond/php-diff
  */
 class Context extends RendererAbstract
--- lib/jblond/Diff/SequenceMatcher.php
+++ lib/jblond/Diff/SequenceMatcher.php
@@ -11,7 +11,7 @@ namespace jblond\Diff;
  * @author Chris Boulton <chris.boulton@interspire.com>
  * @copyright (c) 2009 Chris Boulton
  * @license New BSD License http://www.opensource.org/licenses/bsd-license.php
- * @version 1.12
+ * @version 1.13
  * @link https://github.com/JBlond/php-diff
  */
 class SequenceMatcher
