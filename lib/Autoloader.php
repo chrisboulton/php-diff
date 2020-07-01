69b7e07e2653a6557a0da08009e000183872a716
JBlond <leet31337@web.de>
2020-07-01 12:11:49 +0200

    increase version number

--- lib/Autoloader.php
+++ lib/Autoloader.php
@@ -13,7 +13,7 @@ namespace jblond;
  * @author Mario Brandt <leet31337@web.de>
  * @copyright (c) 2015 Mario Brandt
  * @license New BSD License http://www.opensource.org/licenses/bsd-license.php
- * @version 1.17
+ * @version 1.18
  * @link https://github.com/JBlond/php-diff
  */
 class Autoloader
--- lib/jblond/Diff/Renderer/RendererAbstract.php
+++ lib/jblond/Diff/Renderer/RendererAbstract.php
@@ -16,7 +16,7 @@ use jblond\Diff;
  * @author        Ferry Cools <info@DigiLive.nl>
  * @copyright (c) 2009 Chris Boulton
  * @license       New BSD License http://www.opensource.org/licenses/bsd-license.php
- * @version       1.17
+ * @version       1.18
  * @link          https://github.com/JBlond/php-diff
  */
 abstract class RendererAbstract
